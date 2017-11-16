e6588e07138a40bc5274dd22165fa57cadcbdcb0
Yoshihisa Murakami <muranet+cloud9@gmail.com>
2017-11-16 10:47:52 +0900

    heroku動作確認用

--- tweet.rb
+++ tweet.rb
@@ -1,8 +1,6 @@
 #
-
-#require "rubygems"
-require "twitter"
-require "date"
+#require "twitter"
+#require "date"
 
 @twitter = Twitter::REST::Client.new do |config|
   config.consumer_key    = ENV['TWITTER_CONSUMER_KEY']
