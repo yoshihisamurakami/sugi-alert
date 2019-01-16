956e38fa6ddb3ef2f5b29f873b702107d5c7863c
Yoshihisa Murakami <muranet+cloud9@gmail.com>
2019-01-16 12:25:08 +0900

    twitter_botのテスト作成

--- lib/twitter_bot.rb
+++ lib/twitter_bot.rb
@@ -1,18 +1,17 @@
 require "twitter"
 
 class TwitterBot
-  def initialize
-    @twitter = Twitter::REST::Client.new do |config|
+  def tweet(message)
+    twitter_client.update(message)
+    true
+  end
+
+  def twitter_client
+    Twitter::REST::Client.new do |config|
       config.consumer_key    = ENV['TWITTER_CONSUMER_KEY']
       config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
       config.access_token    = ENV['TWITTER_OAUTH_TOKEN']
       config.access_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
     end
   end
-  
-  def tweet(message)
-    @twitter.update(message)
-    true
-  end
-
 end
