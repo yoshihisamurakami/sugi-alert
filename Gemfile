0f5a21684c72fe9cf8a81e34b8a5179a6e77af50
Yoshihisa Murakami <muranet+cloud9@gmail.com>
2019-03-25 17:07:45 +0900

    simplecov導入

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem "twitter"
 gem "rspec", ">= 3.0.0"
 gem "rspec_junit_formatter"
+gem 'simplecov', require: false, group: :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
+require 'simplecov'
+SimpleCov.start
+
 Dir[File.join(File.dirname(__FILE__), "../lib/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
